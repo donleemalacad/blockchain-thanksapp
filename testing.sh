13432b1c0f55e86741a61dbc99e0e38061144e29
Donlee Malacad <donwalkerdev@gmail.com>
2018-07-17 09:18:04 +0800

    modified testing batch

--- testing.sh
+++ testing.sh
@@ -6,9 +6,6 @@ rm -rf /tmp/thanksapp-* thanksapp
 docker rm -f -v `docker ps -a --no-trunc | grep "thanksapp" | cut -d ' ' -f 1` 2>/dev/null
 docker rmi `docker images --no-trunc | grep "thanksapp" | cut -d ' ' -f 1` 2>/dev/null
 
-# Remove Binary
-rm thanksapp
-
 # Fire docker
 docker-compose up --force-recreate -d
 
